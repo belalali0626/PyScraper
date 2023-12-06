fccaad1227f940ba33a053f8420e1efcf685388c
Belal Ali <77330921+belalali0626@users.noreply.github.com>
2023-12-06 22:46:50 +0000

    Now scrapes 1 page successfully, then stores in dictionary.

--- PlaywrightScrapers/autoTraderScraper.py
+++ PlaywrightScrapers/autoTraderScraper.py
@@ -1,6 +1,5 @@
-import time
 from playwright.sync_api import sync_playwright
-
+import time
 
 def wait_for_element(page, selector, timeout=5000):
     try:
@@ -11,8 +10,7 @@ def wait_for_element(page, selector, timeout=5000):
         print(f"Element with selector '{selector}' not found within the specified timeout.")
         return None
 
-
-def search_in_iframe(page, iframe_selector, selector_to_wait_for,button, button_selector, timeout=5000):
+def search_in_iframe(page, iframe_selector, selector_to_wait_for, button, button_selector, timeout=5000):
     try:
         iframe_handle = wait_for_element(page, iframe_selector, timeout=15000)
 
@@ -28,15 +26,18 @@ def search_in_iframe(page, iframe_selector, selector_to_wait_for,button, button_
                 print(f"Button '{button_selector}' is being clicked")
 
                 click_button(iframe, button_selector, timeout)
+                # Introduce a delay after clicking the button
+                time.sleep(2)
             elif element_handle:
-                    print(f"Cookies text '{selector_to_wait_for}' found in the iframe.")
-                    return True
+                print(f"Cookies text '{selector_to_wait_for}' found in the iframe.")
+                return True
 
         return False
     except Exception as e:
         print(f"Error: {str(e)}")
         print(f"Cookies text '{selector_to_wait_for}' not found in the iframe within the specified timeout.")
         return False
+
 def click_button(target, button_selector, timeout):
     button = target.wait_for_selector(button_selector, timeout=timeout)
 
@@ -53,6 +54,60 @@ def click_button(target, button_selector, timeout):
         print(f"Button '{button_selector}' not found.")
         return False
 
+
+def extract_listing_details(listing_element):
+    title = listing_element.eval_on_selector('h3', 'el => el.textContent')
+    subtitle = listing_element.eval_on_selector('p[data-testid="search-listing-subtitle"]', 'el => el.textContent')
+
+    # Add waiting logic for the price element
+    price = wait_for_selector_content(listing_element, 'div.sc-gvZAcH p.sc-iGgWBj span.sc-bVVIoq', timeout=5000)
+
+    mileage = listing_element.eval_on_selector('span[data-testid="search-listing-seller-mileage"]',
+                                               'el => el.textContent')
+    year = listing_element.eval_on_selector('span[data-testid="search-listing-seller-reg"]', 'el => el.textContent')
+
+    # Extract the listing URL
+    raw_url = listing_element.query_selector('a[data-testid="search-listing-title"]').get_attribute('href')
+
+    # Process values before adding to the dictionary
+    mileage = mileage.replace('Mileage', '').strip() if mileage else None
+    year = year.replace('Year and plate', '').strip() if year else None
+
+    # Clean up the URL
+    base_url = 'https://www.autotrader.co.uk'
+    cleaned_url = f"{base_url}{raw_url.split('?')[0]}"
+
+    # Extract the location text content
+    location_element = listing_element.query_selector('.sc-knuQbY.iPnnQt')
+    location = location_element.text_content() if location_element else None
+    location = location.replace('Dealer location', '').strip() if location else None
+
+    # Create a dictionary with the extracted details
+    listing_details = {
+        'url': cleaned_url,
+        'title': title,
+        'subtitle': subtitle,
+        'price': price,
+        'mileage': mileage,
+        'year': year,
+        'location': location,
+    }
+
+    return listing_details
+
+def wait_for_selector_content(page, selector, timeout=5000):
+    element_handle = None
+    try:
+        element_handle = page.wait_for_selector(selector, timeout=timeout)
+        content = element_handle.text_content()
+        return content.strip() if content else None
+    except Exception as e:
+        print(f"Error waiting for selector '{selector}': {e}")
+    finally:
+        if element_handle:
+            element_handle.dispose()
+    return None
+
 with sync_playwright() as p:
     # Browser setup
     browser = p.chromium.launch(headless=False)
@@ -66,12 +121,35 @@ with sync_playwright() as p:
 
     # Load up the page
     page.goto(
-        'https://www.autotrader.co.uk/car-search?advertising-location=at_cars&include-delivery-option=on&make=BMW'
-        '&model=1%20Series&postcode=NW1%209PQ&year-to=2023')
+        'https://www.autotrader.co.uk/car-search?postcode=NW1%209PQ&year-to=2023&make=BMW&model=1%20Series'
+        '&advertising-location=at_cars&page=1')
 
-    # wait for the iframe, then look for the cookies text within iframe
-    search_in_iframe(page, iframe_selector, cookies_text,True, accept_button, timeout=15000)
+    # wait for the iframe, then look for the cookies text within iframe, then the parameters
+    search_in_iframe(page, iframe_selector, cookies_text, True, accept_button, timeout=15000)
 
+    # find cars
+    page.wait_for_selector('section[data-testid="trader-seller-listing"]')
+
+    # Extract details from each listing on the page
+    listings = page.query_selector_all('section[data-testid="trader-seller-listing"]')
+    all_listing_details = []
+
+    for idx, listing in enumerate(listings, start=1):
+        # Extract details and append to the list
+        details = extract_listing_details(listing)
+        all_listing_details.append(details)
+
+    # Print or process the list of listing details
+    for idx, details in enumerate(all_listing_details, start=1):
+        print(f"Listing {idx} Details:")
+        print(f"Title: {details['title']}")
+        print(f"Subtitle: {details['subtitle']}")
+        print(f"Price: {details['price']}")
+        print(f"Mileage: {details['mileage']}")
+        print(f"Year: {details['year']}")
+        print(f"Location: {details['location']}")
+        print(f"URL: {details['url']}")
+        print("\n")
 
     time.sleep(50)
 
